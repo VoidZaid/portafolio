9474556d6a4dc6122435ddcddee603aa4ba9d701
void_zaid <renediaz725@gmail.com>
2022-11-03 19:18:06 -0500

    funcionalidad de dark mode y almacenado en localstorage para guardar preferencia

--- js/change-icon.js
+++ js/change-icon.js
@@ -1,9 +1,35 @@
 let icon = document.getElementById("icon__mode");
-let arr = ["dark_mode", "light_mode"];
-let now = 0
+let arr = ["light_mode","dark_mode"];
+let now;
 
-icon.addEventListener("click",changeIcon);
-function changeIcon(e){
-    console.log(e.srcElement.innerText = arr[now]);
-    now >= 1? now=0:now++;
+function store(val){
+    localStorage.setItem("darkmode", val)
 }
+function load(){
+    let darkModeVar = localStorage.getItem("darkmode");
+    // console.log(darkModeVar);
+    
+    if(!darkModeVar){
+        now = 0;
+        store(now);
+    }else{
+        now = parseInt(darkModeVar);
+        console.log(now);
+    }
+}
+function changeIcon(e){
+    now >= 1? now=0:now++;
+    e.srcElement.innerText = arr[now];
+    store(now);
+    console.log(now);
+    console.log("hola mundo de nuevo");
+}
+function startChangeIcon(){
+    let icon = document.getElementById("icon__mode");
+    console.log(now)
+    icon.innerText = arr[now];
+}
+
+load();//damos estado incial a variable now
+startChangeIcon();//indicamos que cargue nuestro estilo definido por la variable
+icon.addEventListener("click",changeIcon);
--- js/root-style.js
+++ js/root-style.js
@@ -1,16 +1,23 @@
 
 let file = ["./css/root.css", "./css/root-dark.css"];
-const style = document.querySelector('[href="./css/root.css"]');
+const style = document.querySelector('[href="./css/root-dark.css"]');
+const styleCss = document.getElementById("style");
+console.log(styleCss);
 
-function loadCss(file, now) {
-    // link = document.createElement('link');
-    // link.href = file[disable];
-    // link.type = "text/css";
-    // link.rel = "stylesheet";
-    // document.head.appendChild(link);
+// function loadCss(file, now) {
+//     // link = document.createElement('link');
+//     // link.href = file[disable];
+//     // link.type = "text/css";
+//     // link.rel = "stylesheet";
+//     // document.head.appendChild(link);
 
-    style.href = file[now];
+//     styleCss.href = file[now];
+// }
+// icon.addEventListener("click", ()=>{
+//     loadCss(file,now);
+// })
+function startStyle(){
+    styleCss.href = file[now];
 }
-icon.addEventListener("click", ()=>{
-    loadCss(file,now);
-})
+startStyle();
+icon.addEventListener("click",startStyle)
